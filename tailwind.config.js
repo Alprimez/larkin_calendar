356c109d9fe5381b87837754bae92252432bc7fc
Albert <me@albertutomo.com>
2023-01-15 01:53:12 +0800

    feat: working calendar navigation

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      'sans': ['poppins', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
